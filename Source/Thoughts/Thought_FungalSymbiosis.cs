d5883d6f6f6267269d7436b1c3e38f74e1992d47
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2026-02-27 18:10:23 +0300

    fungal meme

--- Source/Thoughts/Thought_FungalSymbiosis.cs
+++ Source/Thoughts/Thought_FungalSymbiosis.cs
@@ -47,5 +47,66 @@ namespace BiomesGeneticsMushkin
 				return base.MoodOffset();
 			}
 		}
-	}
+    }
+    public class ThoughtWorker_Precept_FungalSymbiosis_Social : ThoughtWorker_Precept_Social
+    {
+        protected override ThoughtState ShouldHaveThought(Pawn p, Pawn otherPawn)
+        {
+            return otherPawn.IsFungal();
+        }
+    }
+    public class ThoughtWorker_Precept_IsFungalSymbiosis : ThoughtWorker_Precept
+    {
+        protected override ThoughtState ShouldHaveThought(Pawn p)
+        {
+            return p.IsFungal();
+        }
+    }
+    public class ThoughtWorker_Precept_FungalSymbiosisColonist : ThoughtWorker_Precept
+    {
+        protected override ThoughtState ShouldHaveThought(Pawn p)
+        {
+            if (p.Faction == null || p.IsFungal())
+            {
+                return ThoughtState.Inactive;
+            }
+            _ = p.Ideo;
+            bool flag = false;
+            foreach (Pawn item in p.MapHeld.mapPawns.SpawnedPawnsInFaction(p.Faction))
+            {
+                if (item.IsFungal())
+                {
+                    flag = true;
+                    Precept_Role precept_Role = item.Ideo?.GetRole(item);
+                    if (precept_Role != null && precept_Role.ideo == p.Ideo && precept_Role.def == PreceptDefOf.IdeoRole_Leader)
+                    {
+                        return ThoughtState.ActiveAtStage(2);
+                    }
+                }
+            }
+            if (flag)
+            {
+                return ThoughtState.ActiveAtStage(1);
+            }
+            return ThoughtState.ActiveAtStage(0);
+        }
+    }
+    public class ThoughtWorker_Precept_FungalSymbiosisPresent : ThoughtWorker_Precept
+    {
+        protected override ThoughtState ShouldHaveThought(Pawn p)
+        {
+            if (p.IsFungal())
+            {
+                return ThoughtState.Inactive;
+            }
+            foreach (Pawn item in p.MapHeld.mapPawns.AllPawnsSpawned)
+            {
+                if (item.IsFungal() && (item.IsPrisonerOfColony || item.IsSlaveOfColony || item.IsColonist))
+                {
+                    return ThoughtState.ActiveDefault;
+                }
+            }
+            return ThoughtState.Inactive;
+        }
+    }
 }
---
+++ Source/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BiomesGeneticsMushkin
+{
+    public static class GeneralUtility
+    {
+
+        public static bool IsFungal(this Pawn p)
+        {
+            return p.genes?.GetFirstGeneOfType<Gene_FungalSymbiosis>() != null;
+        }
+
+    }
+
+}
